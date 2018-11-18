c5fccfcfd9857b2e72425f963a27b6f921084601
774715724@qq.com <handsome0625>
2018-11-18 22:57:50 +0800

    123

---
+++ common/src/main/java/org/apache/rocketmq/common/ConfigManager.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.rocketmq.common;
+
+import org.apache.rocketmq.common.constant.LoggerName;
+import org.apache.rocketmq.logging.InternalLogger;
+import org.apache.rocketmq.logging.InternalLoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * 配置管理抽象基类
+ */
+public abstract class ConfigManager {
+    private static final InternalLogger log = InternalLoggerFactory.getLogger(LoggerName.COMMON_LOGGER_NAME);
+
+    /**
+     * 加载配置类
+     * 先加载 正本，如果正本不存在，再加载副本
+     */
+    public boolean load() {
+        String fileName = null;
+        try {
+            fileName = this.configFilePath();
+            String jsonString = MixAll.file2String(fileName);
+
+            if (null == jsonString || jsonString.length() == 0) {
+                return this.loadBak();
+            } else {
+                this.decode(jsonString);
+                log.info("load " + fileName + " OK");
+                return true;
+            }
+        } catch (Exception e) {
+            log.error("load " + fileName + " failed, and try to load backup file", e);
+            return this.loadBak();
+        }
+    }
+
+    /**
+     * 加载副本
+     */
+    private boolean loadBak() {
+        String fileName = null;
+        try {
+            fileName = this.configFilePath();
+            String jsonString = MixAll.file2String(fileName + ".bak");
+            if (jsonString != null && jsonString.length() > 0) {
+                this.decode(jsonString);
+                log.info("load " + fileName + " OK");
+                return true;
+            }
+        } catch (Exception e) {
+            log.error("load " + fileName + " Failed", e);
+            return false;
+        }
+        return true;
+    }
+
+    public synchronized void persist() {
+        String jsonString = this.encode(true);
+        if (jsonString != null) {
+            String fileName = this.configFilePath();
+            try {
+                MixAll.string2File(jsonString, fileName);
+            } catch (IOException e) {
+                log.error("persist file " + fileName + " exception", e);
+            }
+        }
+    }
+
+    /**
+     * 将对象编码 是否使用格式化
+     */
+    public abstract String encode(final boolean prettyFormat);
+
+    /**
+     * 将jsonString 解码
+     */
+    public abstract void decode(final String jsonString);
+
+    /**
+     * 获取配置信息路径
+     */
+    public abstract String configFilePath();
+
+    /**
+     * 将对象编码
+     */
+    public abstract String encode();
+}
